4e6c2392d4b620b052299428b85b64c1c8b57aa3
KrithinKishor <51524618+KrithinKishor@users.noreply.github.com>
2023-05-09 20:33:56 +0200

    Update Test.py

--- Test.py
+++ Test.py
@@ -6,15 +6,12 @@ from OCC.Display.SimpleGui import init_display
 display, start_display, add_menu, add_function_to_menu = init_display()
 
 
-p1 = gp_Pnt(0,0,0)
-line_dir = gp_Dir(1,1,0)
+p1 = gp_Pnt(0, 0, 0)
+line_dir = gp_Dir(1, 1, 0)
 my_line = Geom_Line(p1, line_dir).Lin()
 my_line = BRepBuilderAPI_MakeEdge(my_line)
 my_line.Build()
 my_line = my_line.Shape()
-
-
-
 
 display.DisplayShape(my_line, update=True)
 start_display()
