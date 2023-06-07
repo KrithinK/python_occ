a0bf8528653d1ae1763bc165995aff154866d8dd
KrithinKishor <96982839+KrithinK@users.noreply.github.com>
2023-06-07 21:43:09 +0200

    file-path

--- Test _Files/projections.py
+++ Test _Files/projections.py
@@ -1,43 +1,18 @@
-# Copyright 2020 Thomas Paviot (tpaviot@gmail.com)
-##
-# This file is part of pythonOCC.
-##
-# pythonOCC is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-##
-# pythonOCC is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser General Public License for more details.
-##
-# You should have received a copy of the GNU Lesser General Public License
-# along with pythonOCC.  If not, see <http://www.gnu.org/licenses/>.
-
-""" Example for shape overlapping detection.
-See pythonocc-core issue #628 at
-https://github.com/tpaviot/pythonocc-core/issues/628
-"""
-
-from OCC.Core.gp import gp_Pnt
-from OCC.Core.BRepPrimAPI import BRepPrimAPI_MakeBox
+import os
 from OCC.Core.BRepMesh import BRepMesh_IncrementalMesh
 from OCC.Core.BRepExtrema import BRepExtrema_ShapeProximity
 from OCC.Display.SimpleGui import init_display
 from OCC.Core.STEPControl import STEPControl_Reader
 import OCC.Core.TopExp as TopExp
 import OCC.Core.TopAbs as TopAbs
-from OCC.Core.gp import gp_Ax2, gp_Pnt, gp_Dir, gp_Ax1, gp_Trsf, gp_Vec
+from OCC.Core.gp import gp_Trsf, gp_Vec
 from OCC.Core.BRepBuilderAPI import (
     BRepBuilderAPI_Transform,
-    BRepBuilderAPI_MakeWire,
-    BRepBuilderAPI_MakeEdge,
-    BRepBuilderAPI_MakeFace,
 )
 
 step_reader = STEPControl_Reader()
-status = step_reader.ReadFile("final_assembly.stp")
+status = step_reader.ReadFile(os.getcwd()[:-12]+"/Assets/final_assembly.stp")
+print(os.getcwd()[:-12]+"/Assets/final_assembly.stp")
 # if status == IFSelect_RetDone:
 step_reader.TransferRoots()
 shape = step_reader.Shape()
@@ -57,7 +32,7 @@ while exp.More():
 box1 = sub_assemblies[3]
 box2 = sub_assemblies[0]
 
-for i in range(10):
+for i in range(1):
     move.SetTranslation(gp_Vec(i, 0, 0))
     box2 = BRepBuilderAPI_Transform(
         box2, move, True).Shape()
@@ -91,3 +66,11 @@ for i in range(10):
         shape_2_faces.append(face)
     print(i)
     print(len(shape_1_faces))
+
+    display, start_display, add_menu, add_function_to_menu = init_display()
+    display.DisplayShape(box1, transparency=0.5)
+    display.DisplayShape(box2, transparency=0.5)
+    display.DisplayShape(shape_1_faces + shape_2_faces, color="RED")
+
+    display.FitAll()
+    start_display()
