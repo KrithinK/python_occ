9f9f5447fbfcc015e4002dc90b7ab94c1141f885
KrithinKishor <96982839+KrithinK@users.noreply.github.com>
2023-08-16 19:31:21 +0200

    multi threading

--- Test _Files/temporary_multi.py
+++ Test _Files/temporary_multi.py
@@ -441,6 +441,39 @@ def end_time():
     print(end - start)
 
 
+def allDirections(final, sub, i):
+    print(bcolors.OKBLUE + "This is the arroach direction of block " +
+          str(i) + bcolors.ENDC)
+    bb1 = get_boundingbox(sub)
+    bb = get_boundingbox(final)
+    pos_posy = Process(target=positive_y, args=(
+        bb1, bb, final, sub))
+    pos_negy = Process(target=negative_y, args=(
+        bb1, bb, final, sub))
+    pos_posx = Process(target=positive_x, args=(
+        bb1, bb, final, sub))
+    pos_negx = Process(target=negative_x, args=(
+        bb1, bb, final, sub))
+    pos_posz = Process(target=positive_z, args=(
+        bb1, bb, final, sub))
+    pos_negz = Process(target=negative_z, args=(
+        bb1, bb, final, sub))
+
+    pos_posy.start()
+    pos_negy.start()
+    pos_posx.start()
+    pos_negx.start()
+    pos_posz.start()
+    pos_negz.start()
+
+    pos_posy.join()
+    pos_negy.join()
+    pos_posx.join()
+    pos_negx.join()
+    pos_posz.join()
+    pos_negz.join()
+
+
 if __name__ == '__main__':
     step_reader = STEPControl_Reader()
     status = step_reader.ReadFile(
@@ -460,43 +493,27 @@ if __name__ == '__main__':
         exp.Next()
 
     temp = sub_assemblies[0]
-    sub_assemblies_cummul = sub_assemblies[0]
-    sub_assemblies_reorder = sub_assemblies[0]
+    sub_assemblies_cummul = []
+    sub_assemblies_reorder = []
+    sub_assemblies_cummul.append(sub_assemblies[0])
+    sub_assemblies_reorder.append(sub_assemblies[0])
     bb = get_boundingbox(temp)
+    counter = 1
+    Processes = []
 
     for i in [5, 6, 1, 2, 3, 4, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]:
-        print(bcolors.OKBLUE + "This is the arroach direction of block " +
-              str(i) + bcolors.ENDC)
-        bb1 = get_boundingbox(sub_assemblies[i])
-        bb = get_boundingbox(temp)
-        pos_posy = Process(target=positive_y, args=(
-            bb1, bb, temp, sub_assemblies[i]))
-        pos_negy = Process(target=negative_y, args=(
-            bb1, bb, temp, sub_assemblies[i]))
-        pos_posx = Process(target=positive_x, args=(
-            bb1, bb, temp, sub_assemblies[i]))
-        pos_negx = Process(target=negative_x, args=(
-            bb1, bb, temp, sub_assemblies[i]))
-        pos_posz = Process(target=positive_z, args=(
-            bb1, bb, temp, sub_assemblies[i]))
-        pos_negz = Process(target=negative_z, args=(
-            bb1, bb, temp, sub_assemblies[i]))
+        sub_assemblies_cummul.append(BRepAlgoAPI_Fuse(
+            sub_assemblies_cummul[len(sub_assemblies_cummul)-1], sub_assemblies[i]).Shape())
+        sub_assemblies_reorder.append(sub_assemblies[i])
 
-        pos_posy.start()
-        pos_negy.start()
-        pos_posx.start()
-        pos_negx.start()
-        pos_posz.start()
-        pos_negz.start()
+    for i in range(len(sub_assemblies_cummul)):
+        p = Process(target=allDirections, args=(
+            sub_assemblies_cummul[i-1], sub_assemblies_reorder[i], counter))
+        counter += 1
+        p.start()
+        Processes.append(p)
 
-        pos_posy.join()
-        pos_negy.join()
-        pos_posx.join()
-        pos_negx.join()
-        pos_posz.join()
-        pos_negz.join()
-
-        temp = BRepAlgoAPI_Fuse(
-            temp, sub_assemblies[i]).Shape()
+    for process in Processes:
+        process.join()
 
     end_time()
